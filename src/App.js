d53e55f91405731f8e39d5f49fe171bfa9e5eea0
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-10-29 21:20:59 -0300

    feat (Dashboard): new dashboad component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import Login from 'components/Login';
+import Dashboard from 'components/Dashboard';
 
 const App = () => {
   const [user, setUser] = useState(null);
 
   if (!user) return <Login setUser={setUser} />;
-  return <h1>Dashboard</h1>;
+  return <Dashboard />;
 };
 
 export default App;
