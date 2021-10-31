34c89760d78d6fb7eabc60ab2625b7221cc3e573
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-10-31 20:02:06 -0300

    refactor (services): removing explicit arrow function return

--- src/services/api.js
+++ src/services/api.js
@@ -1,27 +1,23 @@
 import axios from 'axios';
 
-export const apiURL = 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon';
+export const apiURL =
+  'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon';
 
 export const api = axios.create({
   baseURL: apiURL,
 });
 
-export const apiGetAllDragons = async () => {
-  return await api.get();
-};
+export const apiGetAllDragons = async () => 
+  await api.get();
 
-export const apiGetDragon = async (id) => {
-  return await api.get(`/${id}`);
-};
+export const apiGetDragon = async (id) => 
+  await api.get(`/${id}`);
 
-export const apiEditDragon = async (id, values) => {
-  return await api.put(`/${id}`, values);
-};
+export const apiEditDragon = async (id, values) =>
+  await api.put(`/${id}`, values);
 
-export const apiCreateNewDragon = async (values) => {
-  return await api.post('', values);
-};
+export const apiCreateNewDragon = async (values) => 
+  await api.post('', values);
 
-export const apiDeleteDragon = async (id) => {
-  return await api.delete(`/${id}`);
-};
+export const apiDeleteDragon = async (id) => 
+  await api.delete(`/${id}`);
